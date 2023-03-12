564338a6f3a50623fe02ed079e6bfc15be9256c9
Rene Grob <rene.grob76@gmail.com>
2023-03-12 18:41:33 +0100

    Add confetti

--- app/src/main/java/com/github/viviennezoe/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/github/viviennezoe/guessthenumber/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.viviennezoe.guessthenumber
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.github.jinatonic.confetti.CommonConfetti
 
 const val TAG = "MainActivity"
 
@@ -18,9 +20,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (theNumber < 0) {
-            theNumber = (1..100).random()
-            Log.i(TAG, "theNumber = $theNumber")
+            shuffleNewNumber()
         }
+    }
+
+    private fun shuffleNewNumber() {
+        theNumber = (1..100).random()
+        Log.i(TAG, "theNumber = $theNumber")
     }
 
     fun onGuessNumber(view: View) {
@@ -36,6 +42,12 @@ class MainActivity : AppCompatActivity() {
                 iconCorrect.visibility = View.VISIBLE
                 iconWrong.visibility = View.INVISIBLE
                 tip.text = ""
+                shuffleNewNumber()
+                val colors = intArrayOf( Color.BLUE, Color.YELLOW, Color.RED, Color.GREEN )
+                tip.text = "$guess ist richtig!"
+                CommonConfetti
+                    .rainingConfetti(findViewById(R.id.container), colors)
+                    .oneShot();
             } else {
                 iconCorrect.visibility = View.INVISIBLE
                 iconWrong.visibility = View.VISIBLE
@@ -44,8 +56,8 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     tip.text = "$guess ist zu gross"
                 }
-                edtNumber.text.clear()
             }
+            edtNumber.text.clear()
         }
     }
 }
